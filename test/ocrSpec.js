32d489205f112ba7c4303a9273db220708df6c44
Marc Peabody <marcpeabody@yahoo.com>
2017-02-04 22:47:33 -0500

    handle looking up just a single ocr number for now

--- test/ocrSpec.js
+++ test/ocrSpec.js
@@ -1,7 +1,21 @@
 const {expect} = require('chai');
+const {numberByOCRText} = require('../lib/ocr');
 
 describe('OCR', function() {
-  it('does nothing yet', function() {
-    expect(true).to.equal(true);
+  it('find single zero by text', function() {
+    // assume we are only looking for one number
+    // assume the fourth blank line is missing
+    const zero = " _ " +
+                 "| |" +
+                 "|_|";
+    const actual = numberByOCRText(zero)
+    expect(actual).to.equal(0);
+  });
+  it('find single five by text', function() {
+    const five = " _ " +
+                 "|_ " +
+                 " _|";
+    const actual = numberByOCRText(five)
+    expect(actual).to.equal(5);
   });
 });
